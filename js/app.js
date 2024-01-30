f9f8391a38435ca50bdd0a4088441c12772d2d6d
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2024-01-30 09:58:37 +0100

    added functionality to sidebar

---
+++ js/app.js
@@ -0,0 +1,39 @@
+const sidebarButtons = document.querySelectorAll('.nav-button')
+const pages = document.querySelectorAll('.page')
+
+
+
+sidebarButtons.forEach(sidebarButton => {
+	function ShowPages(event) {
+		
+		const clickedButton = event.currentTarget
+		const pageToDisplay = clickedButton.dataset.buttons
+
+		pages.forEach(page => {
+			page.classList.remove("visible")
+	
+			if (page.dataset.pages === pageToDisplay) {
+				page.classList.add("visible")
+			}
+		})
+	
+
+	
+	}
+
+	sidebarButton.addEventListener("click", ShowPages)
+})
+
+
+const fetchData = async () => {
+	try {
+		const response = await fetch('https://swapi.dev/api/films/1/')
+		const data = await response.json()
+		console.log(data);
+	} catch (error) {
+		console.log("Something went wrong!", error);
+	}
+	
+}
+
+fetchData()
