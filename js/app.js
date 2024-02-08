9ac4f6b45ecb409aff712525267e5dbe57186160
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2024-02-08 17:24:15 +0100

    fixed the big design issues

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const films = document.querySelector('.page-films')
 const people = document.querySelector('.page-people')
 const planets = document.querySelector('.page-planets')
 const vehicles = document.querySelector('.page-vehicles')
+const mainContent = document.querySelector('.main')
+
 
 
 
@@ -15,14 +17,30 @@ sidebarButtons.forEach(sidebarButton => {
 		const clickedButton = event.currentTarget
 		const pageToDisplay = clickedButton.dataset.buttons
 
+
+		sidebarButtons.forEach(button => {
+
+			button.classList.remove("nav-buttons--active")
+	
+			if (button.dataset.buttons === pageToDisplay){
+				button.classList.add("nav-buttons--active")
+			}
+		})
+
+
 		pages.forEach(page => {
 			page.classList.remove("visible")
 	
 			if (page.dataset.pages === pageToDisplay) {
 				page.classList.add("visible")
 			}
+
+			
 		})
-	
+
+		
+		
+		
 
 		fetchData(pageToDisplay)
 	}
@@ -53,86 +71,96 @@ const fetchData = async (attribute) => {
 
 
 
-
 const renderFilms = (info) => {
-	
-	const ul = document.createElement('ul')
-	ul.classList.add('ul-content')
-	films.appendChild(ul)
-
-	ul.innerHTML = ""
+	// ul.innerHTML = ""
 	info.forEach(element => {
-		const li = document.createElement('li')
+		
+		const divContainer = document.createElement('div')
+		films.appendChild(divContainer)
 
-		const title = document.createElement('span')
-		const director = document.createElement('span')
-		const releaseDate = document.createElement('span')
-		const producer = document.createElement('span')
-		const picture = document.createElement('span')
+		const title = document.createElement('div')
+		const director = document.createElement('div')
+		const releaseDate = document.createElement('div')
+		const producer = document.createElement('div')
+
+		const textContent = document.createElement('div')
+		const picture = document.createElement('div')
 		const image = document.createElement('img')
 
-		li.classList.add('userItem')
-		title.classList.add('films-title')
-		director.classList.add('films-director')
-		releaseDate.classList.add('films-releaseDate')
-		producer.classList.add('films-producer')
-		picture.classList.add('films-picture')
+		divContainer.classList.add('card')
+		picture.classList.add('picture-content')
+		title.classList.add('text-content')
+		director.classList.add('text-content')
+		releaseDate.classList.add('text-content')
+		producer.classList.add('text-content')
+		textContent.classList.add('text-container')
 
-		ul.appendChild(li)
-		li.append(title, director, releaseDate, producer, picture)
+		
+		divContainer.append(picture, textContent)
+		textContent.append(title, director, releaseDate, producer)
 		picture.append(image)
 
 		title.textContent = element.title
 		director.textContent = element.director
 		releaseDate.textContent = element.release_date
 		producer.textContent = element.producer
-		// image.src = element.avatar
+
+		image.src = `./assets/film/${element.title}.jpg`
 		
 	})
 }
 
 const renderPeople = (info) => {
-	people.innerHTML = ""
-	const ul = document.createElement('ul')
-	people.appendChild(ul)
-	for (let i = 0; i < 6; i++) {
-		const li = document.createElement('li')
+	
+	info.forEach(element => {
+		const divContainer = document.createElement('div')
+		people.appendChild(divContainer)
 
 		const name = document.createElement('span')
 		const height = document.createElement('span')
 		const hairColor = document.createElement('span')
 		const eyeColor = document.createElement('span')
 		const gender = document.createElement('span')
+
+		const textContent = document.createElement('div')
 		const picture = document.createElement('span')
 		const image = document.createElement('img')
 
-		// li.classList.add('userItem')
-		// title.classList.add('films-title')
-		// director.classList.add('films-director')
-		// releaseDate.classList.add('films-releaseDate')
-		// producer.classList.add('films-producer')
-		// picture.classList.add('films-picture')
+		divContainer.classList.add('card')
+		picture.classList.add('picture-content')
+		name.classList.add('text-content')
+		height.classList.add('text-content')
+		hairColor.classList.add('text-content')
+		eyeColor.classList.add('text-content')
+		gender.classList.add('text-content')
+		textContent.classList.add('text-container')
+		
 
-		ul.appendChild(li)
-		li.append(name, height, hairColor, eyeColor, gender)
+		divContainer.append(picture, textContent)
+		textContent.append(name, height, hairColor, eyeColor, gender)
 		picture.append(image)
 
-		name.textContent = info[i].name
-		height.textContent = info[i].height
-		hairColor.textContent = info[i].hair_color
-		eyeColor.textContent = info[i].eye_color
-		gender.textContent = info[i].gender
-		// image.src = element.avatar
+		name.textContent = element.name
+		height.textContent = element.height
+		hairColor.textContent = element.hair_color
+		eyeColor.textContent = element.eye_color
+		gender.textContent = element.gender
 		
-	}
+		
+		if(["Biggs Darklighter", "Leia Organa", "Owen Lars", "R5-D4"].includes(element.name)) {
+			image.src = `./assets/people/${element.name}.webp`
+		} else {
+			image.src = `./assets/people/${element.name}.jpeg`
+		}
+		
+	})
 }
 
 const renderPlanets = (info) => {
-	planets.innerHTML = ""
-	const ul = document.createElement('ul')
-	planets.appendChild(ul)
-	for (let i = 0; i < 6; i++) {
-		const li = document.createElement('li')
+
+	info.forEach(element => {
+		const divContainer = document.createElement('div')
+		planets.appendChild(divContainer)
 
 		const name = document.createElement('span')
 		const population = document.createElement('span')
@@ -140,36 +168,45 @@ const renderPlanets = (info) => {
 		const terrain = document.createElement('span')
 		const diameter = document.createElement('span')
 
+		const textContent = document.createElement('div')
 		const picture = document.createElement('span')
 		const image = document.createElement('img')
 
-		// li.classList.add('userItem')
-		// title.classList.add('films-title')
-		// director.classList.add('films-director')
-		// releaseDate.classList.add('films-releaseDate')
-		// producer.classList.add('films-producer')
-		// picture.classList.add('films-picture')
+		divContainer.classList.add('card')
+		picture.classList.add('picture-content')
+		name.classList.add('text-content')
+		population.classList.add('text-content')
+		climate.classList.add('text-content')
+		terrain.classList.add('text-content')
+		diameter.classList.add('text-content')
+		textContent.classList.add('text-container')
 
-		ul.appendChild(li)
-		li.append(name, population, climate, terrain, diameter)
+		divContainer.append(picture, textContent)
+		textContent.append(name, population, climate, terrain, diameter)
 		picture.append(image)
 
-		name.textContent = info[i].name
-		population.textContent = info[i].population
-		climate.textContent = info[i].climate
-		terrain.textContent = info[i].terrain
-		diameter.textContent = info[i].diameter
-		// image.src = element.avatar
+		name.textContent = element.name
+		population.textContent = element.population
+		climate.textContent = element.climate
+		terrain.textContent = element.terrain
+		diameter.textContent = element.diameter
+
+		if(["Coruscant", "Endor"].includes(element.name)) {
+			image.src = `./assets/planet/${element.name}.jpeg`
+		} else {
+			image.src = `./assets/planet/${element.name}.webp`
+		}
+
+		console.log(element.name);
 		
-	}
+	})
 }
 
 const renderVehicles = (info) => {
-	vehicles.innerHTML = ""
-	const ul = document.createElement('ul')
-	vehicles.appendChild(ul)
-	for (let i = 0; i < 6; i++) {
-		const li = document.createElement('li')
+
+	info.forEach(element => {
+		const divContainer = document.createElement('div')
+		vehicles.appendChild(divContainer)
 
 		const name = document.createElement('span')
 		const model = document.createElement('span')
@@ -177,25 +214,39 @@ const renderVehicles = (info) => {
 		const crew = document.createElement('span')
 		const maxSpeed = document.createElement('span')
 
+		const textContent = document.createElement('div')
 		const picture = document.createElement('span')
 		const image = document.createElement('img')
 
-		// li.classList.add('userItem')
-		// title.classList.add('films-title')
-		// director.classList.add('films-director')
-		// releaseDate.classList.add('films-releaseDate')
-		// producer.classList.add('films-producer')
-		// picture.classList.add('films-picture')
+		divContainer.classList.add('card')
+		picture.classList.add('picture-content')
+		name.classList.add('text-content')
+		model.classList.add('text-content')
+		manufacturer.classList.add('text-content')
+		crew.classList.add('text-content')
+		maxSpeed.classList.add('text-content')
+		textContent.classList.add('text-container')
 
-		ul.appendChild(li)
-		li.append(name, model, manufacturer, crew, maxSpeed)
+		
+		divContainer.append(picture, textContent)
+		textContent.append(name, model, manufacturer, crew, maxSpeed)
 		picture.append(image)
 
-		name.textContent = info[i].name
-		model.textContent = info[i].model
-		manufacturer.textContent = info[i].manufacturer
-		crew.textContent = info[i].crew
-		maxSpeed.textContent = info[i].max_atmosphering_speed
-		// image.src = element.avatar
-	}	
+		name.textContent = element.name
+		model.textContent = element.model
+		manufacturer.textContent = element.manufacturer
+		crew.textContent = element.crew
+		maxSpeed.textContent = element.max_atmosphering_speed
+		console.log(element);
+		
+		if(element.name === "TIE bomber") {
+			image.src = `./assets/vehicles/${element.name}.avif`
+		} else if(["AT-AT", "AT-ST", "T-16 skyhopper"].includes(element.name)){
+			image.src = `./assets/vehicles/${element.name}.jpeg`
+		} else if(element.name === "TIE/LN starfighter") {
+			image.src = `./assets/vehicles/starfighter.jpeg`
+		} else {
+			image.src = `./assets/vehicles/${element.name}.webp`
+		}
+	})
 }	
